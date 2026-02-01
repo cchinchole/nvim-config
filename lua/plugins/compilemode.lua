eaacc5e05b0a11f6619e6b210eaac476d88f65ee
cchinchole <7995419+cchinchole@users.noreply.github.com>
2026-02-01 10:20:19 -0600

    Fixed compilemode Forgot to add the command for compile mode

--- lua/plugins/compilemode.lua
+++ lua/plugins/compilemode.lua
@@ -5,14 +5,18 @@ return {
         "nvim-lua/plenary.nvim",
     },
     lazy = true,
+    cmd = 'Compile',
     config = function()
         ---@type CompileModeOpts
         vim.g.compile_mode = {
             input_word_completion = true,
             default_command = {
-                c = "cc -o %:r % && ./%:r",
-                cpp = "cc -std=c++23 -o %:r % && ./%:r",
-                python = "python %",
+                --c = "cc -o %:r % && ./%:r",
+                --cpp = "cc -std=c++23 -o %:r % && ./%:r",
+                c = "make -k",
+                cpp = "make -k",
+                python = "python main",
+                go = "go run .",
             },
             focus_compilation_buffer = true
         }
